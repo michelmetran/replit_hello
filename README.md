1571df1b9a1931d916efc77a1ae4683e731bb9d9
Michel Metran <michelmetran@gmail.com>
2023-03-28 00:56:14 -0300

    s

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Repli.it
+
+
 An example of a HTTP server that serves a "Hello World" message.
 
 [Learn more here.](https://examples.deno.land/http-server)
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 function handler(_req: Request): Response {
-  return new Response("Hello, World!");
+  return new Response("Aooow Mundo véio sem portera!!");
 }
 console.log("Listening on http://localhost:8000");
 serve(handler);
